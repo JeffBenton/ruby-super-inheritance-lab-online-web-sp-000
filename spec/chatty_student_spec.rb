b3f7660953b8ffbfc0446fa9e70c0bf866a82a4e
Jeff Benton <sikletz@gmail.com>
2019-02-22 07:15:12 +0000

    Automatically backed up by Learn

--- spec/chatty_student_spec.rb
+++ spec/chatty_student_spec.rb
@@ -6,7 +6,7 @@ describe "ChattyStudent" do
   
   describe "#hello" do
     it "returns a greeting phrase plus a long chatty phrase" do  
-      expect{chatty_student.hello}.to output("Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n").to_stdout
+      expect{chatty_student.hello}.to output("Hey there! I'm so excited to learn stuff. How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...").to_stdout
     end
   end
 
